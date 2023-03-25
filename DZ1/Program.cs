ddb258b16de36ed0183a359c91177fd04942bc27
Admin <ge1983@yandex.ru>
2023-03-25 16:46:59 +1000

    Добавлено задача 1 для ДЗ

---
+++ DZ1/Program.cs
@@ -0,0 +1,41 @@
+﻿// 
+int[] InputArray (int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int findToArray (int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0 )
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+Console.WriteLine("Введите количество элеменов");
+int length = Convert.ToInt32(Console.ReadLine()); 
+int[] array = InputArray(length);
+Console.WriteLine("Введеные числа: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество положительных введеных чисел - {findToArray (array)}");
+
